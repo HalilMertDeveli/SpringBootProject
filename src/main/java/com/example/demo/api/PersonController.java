f73f81d268f7f6f5d295ae69007db9470387b0da
haliko <halilmertdeveliii@gmail.com>
2023-07-19 13:05:31 +0300

    we got all people in our locak servise and done the select person opeartion

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,10 +3,11 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
 @RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
@@ -19,5 +20,13 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
+    }
+    @GetMapping//for request of get
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+    @GetMapping(path = "/{id}")//i passed the id to path
+    public Person getPersonById(@PathVariable("id") UUID id){//id passing with PathVariable
+        return personService.getPersonById(id).orElse(null);
     }
 }
