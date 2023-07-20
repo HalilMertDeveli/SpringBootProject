b60118f7aee76a1a93af7c9a0ce066f0c041ea7d
haliko <halilmertdeveliii@gmail.com>
2023-07-20 21:12:16 +0300

    i addded constraint

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,8 +3,9 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,28 +14,34 @@ import java.util.UUID;
 public class PersonController {
 
     private final PersonService personService;
+
     @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+
     @PostMapping
-    public void addPerson(@RequestBody Person person){
+    public void addPerson(@Valid @NonNull @RequestBody Person person) {
         personService.addPerson(person);
     }
+
     @GetMapping//for request of get
-    public List<Person> getAllPeople(){
+    public List<Person> getAllPeople() {
         return personService.getAllPeople();
     }
+
     @GetMapping(path = "/{id}")//i passed the id to path
-    public Person getPersonById(@PathVariable("id") UUID id){//id passing with PathVariable
+    public Person getPersonById(@PathVariable("id") UUID id) {//id passing with PathVariable
         return personService.getPersonById(id).orElse(null);
     }
+
     @DeleteMapping(path = "{id}")
-    public void deletePersonById(@PathVariable("id") UUID id){
+    public void deletePersonById(@PathVariable("id") UUID id) {
         personService.deletePerson(id);
     }
+
     @PutMapping(path = "{id}")
-    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person person){
-        personService.updatePerson(id,person);
+    public void updatePerson(@PathVariable("id") UUID id,@Valid @NonNull @RequestBody Person person) {
+        personService.updatePerson(id, person);
     }
 }
